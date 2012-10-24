1337a36dbbfaf73c04317a48e921a94f345ecd75
inmarelibero <inmarelibero@gmail.com>
2012-10-24 21:39:15 +0200

    user reserved area refactored

---
+++ src/Bricks/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bricks\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+/**
+ * @Route("/user")
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/dashboard", name="user_dashboard")
+     * @Template()
+     */
+    public function dashboardAction()
+    {
+        return array();
+    }
+}
