0b0f4964b7e0d7d2377e48f37e35f2d15cdf7093
inmarelibero <inmarelibero@gmail.com>
2012-11-19 23:08:02 +0100

    added modal login box

---
+++ src/Bricks/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bricks\UserBundle\Controller;
+
+use FOS\UserBundle\Controller\SecurityController as BaseController;
+
+class SecurityController extends BaseController
+{
+    /**
+     * print the code for the modal login box
+     */
+    public function _modalLoginAction()
+    {
+        $csrfToken = $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate');
+        
+        return $this->container->get('templating')->renderResponse('BricksUserBundle:Security:_modalLogin.html.twig', array(
+            'csrf_token' => $csrfToken
+        ));
+    }
+}
