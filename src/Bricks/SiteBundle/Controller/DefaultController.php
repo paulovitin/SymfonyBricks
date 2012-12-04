9568e3708fa8c38e96c2dd38d684651d6e99a163
inmarelibero <inmarelibero@gmail.com>
2012-12-04 12:19:04 +0100

    added changelog page

--- src/Bricks/SiteBundle/Controller/DefaultController.php
+++ src/Bricks/SiteBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\Finder\Finder;
+
 class DefaultController extends Controller
 {
     /**
@@ -30,5 +32,27 @@ class DefaultController extends Controller
     public function contributeAction()
     {
         return array();
+    }
+    
+    /**
+     * @Route("/changelog", name="changelog")
+     * @Template()
+     */
+    public function changelogAction()
+    {
+        $finder = new Finder();
+        $finder->files()
+            ->in($this->container->getParameter('kernel.root_dir').'/../')
+            ->depth('== 0')
+            ->name('CHANGELOG.md')
+        ;
+        
+        foreach ($finder as $file) {
+            $changelog_content = file_get_contents($file->getRealPath());
+        }
+        
+        return array(
+            'changelog_content' => $changelog_content
+        );
     }
 }
