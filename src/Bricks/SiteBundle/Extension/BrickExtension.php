6255dd14641fa21404cc1a4c5fae18a42a6415ba
inmarelibero <inmarelibero@gmail.com>
2013-01-10 20:34:01 +0100

    added code highlight to a brick installed nicodmf / HighlightBundle

--- src/Bricks/SiteBundle/Extension/BrickExtension.php
+++ src/Bricks/SiteBundle/Extension/BrickExtension.php
@@ -2,6 +2,7 @@
 namespace Bricks\siteBundle\Extension;
 
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\DomCrawler\Crawler;
 use Bricks\SiteBundle\Entity\Brick;
 
 /**
@@ -10,21 +11,24 @@ use Bricks\SiteBundle\Entity\Brick;
 class BrickExtension extends \Twig_Extension
 {
     private $router;
+    private $tiwg;
    
-    public function __construct(Router $router)
+    public function __construct(Router $router, $twig)
     {
         $this->router = $router;
+        $this->twig = $twig;
     }
     
     public function getFunctions()
     {
         return array(
             'brick_formatted_tags' => new \Twig_Function_Method($this, 'brickFormattedTags'),
+            'brick_highlight_code' => new \Twig_Function_Method($this, 'brickHighlightCode'),
         );
     }
     
     /**
-     * print a string of tag titles separated by $separator
+     * print a string of Brick tag titles separated by $separator
      * 
      * @param Brick $brick Brick object
      * @param string $separator string to separate tag titles
@@ -55,6 +59,38 @@ EOD;
         }
 
         return $output;
+    }
+
+    /**
+     * Highlight the content of <pre> tags of $source string via HighlightBundle
+     *
+     * @param $source string containing <pre> tags to be highlighted
+     * @return string string containing <pre> tags with highlighted code
+     */
+    public function brickHighlightCode($source)
+    {
+        $highlighter = $this->twig->getExtension('twig.extension.highlight');
+
+        // create a DOMDocument to replace the content of <pre> tags with highlighted code
+        $doc = new \DOMDocument();
+
+        // load $source string as html
+        $doc->loadHTML($source);
+
+        $xpath = new \DOMXPath($doc);
+
+        // replace the content of <pre> tags with highlighted code
+        foreach ($xpath->query('//pre') as $node) {
+
+            $highlightedSource = ($highlighter->highlight(trim($node->firstChild->nodeValue), 'html'));
+
+            $highlightedDomFragment = $doc->createDocumentFragment();
+            $highlightedDomFragment->appendXML("<![CDATA[{$highlightedSource}]]>");
+
+            $node->parentNode->replaceChild($highlightedDomFragment, $node);
+        }
+
+        return $doc->saveHTML();
     }
 
     public function getName()
